9075ba887d3475f0c8a3e84eb1f84671a18865f0
therev123 <bryantremblay321@hotmail.com>
2014-12-07 22:28:13 -0500

    Comit 2 -ajout des variables entières nbrUser et nbrMystere nbrUser: Variable qui entrepose le nombre entrez par l'utilisateur nbrMystere: Variable qui entrepose le nombre mystère ajout d'une condition testant si le nombre du joueur est équivalent au nombre mystere

--- Nombre Mystère/Nombre Mystère/main.cpp
+++ Nombre Mystère/Nombre Mystère/main.cpp
@@ -3,7 +3,21 @@ using namespace std;
 
 int main(int agrc, char** argv)
 {
+	int nbrUser;
+	int nbrMystere = 5;
+
 	cout << "Bienvenu dans le jeu Nombre Mystere!!!\n";
+	cout << "Entrez un nombre entre 0 et 10: ";
+	cin >> nbrUser;
+	//Teste si le nombre entré par l'utilisateur est semblable au nombre mystère
+	if (nbrUser == nbrMystere)
+	{
+		cout << "Bravo tu as trouver le nombre mystère!!!\n";
+	}
+	else
+	{
+		cout << "Meilleure chance la prochaine fois\n";
+	}
 	system("pause");
 	return 0;
 }
